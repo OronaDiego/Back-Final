755d5b737bcd5168dc4f14eeaecfdd7d32bf5146
Diego Orona <tuemail@gmail.comdiegosebastianorona@gmail.com>
2026-01-07 17:36:59 -0300

    swagger

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 import usersRouter from "./routes/user-routes.js";
 import productsRouter from "./routes/products-routes.js";
 import { loggerMiddleware } from "./middlewares/logger-middleware.js";
+import { swaggerSetup } from "./config/swagger.js";
+import { errorHandler } from "./middlewares/error-middleware.js";
 
 dotenv.config();
 
@@ -13,9 +15,12 @@ app.use(express.json());
 app.use(loggerMiddleware);
 app.use("/api/users", usersRouter);
 app.use("/api/products", productsRouter);
+app.get("/", (req, res) => {
+  res.send("API Backend Final funcionando");
+});
 
+swaggerSetup(app);
 
-import { errorHandler } from "./middlewares/error-middleware.js";
 
 app.use(errorHandler);
 
